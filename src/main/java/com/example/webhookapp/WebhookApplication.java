5f6acd630d17cddf1231e8c4da56c162e75065ae
Sasidhar-007 <parisesasidharsai007@gmail.com>
2025-08-29 11:51:28 +0530

    Update WebhookApplication.java

--- src/main/java/com/example/webhookapp/WebhookApplication.java
+++ src/main/java/com/example/webhookapp/WebhookApplication.java
@@ -1,5 +1,6 @@
 package com.example.webhookapp;
 
+import com.example.webhookapp.service.WebhookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
